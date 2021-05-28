3e5a09e3e4b67b3e70bfe1ddda7e4d6fac64c72b
Non-Unruly <shihoongbo@163.com>
2021-05-28 12:02:45 +0800

    更新了日志

--- Example/main.cpp
+++ Example/main.cpp
@@ -1,19 +1,16 @@
 #include <cstdio>
-#include "../ssFormatPlus.h"
 #include <iostream>
 #include <vector>
 #include <list>
 #include <string>
 
+#include "../ssFormatPlus.h"
+#include "../ssLogger.h"
+
+
 int main()
 {
-	std::vector<int> nums = { 1,2,3,4,5 };
-	std::vector<std::string> stdstrs = {"hbshi","hbshi3","weiwang70"};
-	std::vector<const char*> strs = { "what","do","I","want" };
 
-	printf("%s\n", ssTools::ssFormatVecInt(nums).c_str());
-	printf("%s\n", ssTools::ssFormatVecStdStr(stdstrs).c_str());
-	printf("%s\n", ssTools::ssFormatVecStr(strs).c_str());
 	system("pause");
 	return 0;
 }
