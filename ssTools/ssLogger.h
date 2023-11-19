64a6820f6b1f46154119d91b328950e8a4656288
Non-Unruly <shihoongbo@163.com>
2023-11-19 18:53:11 +0800

    调整ssLogger模块，返回日志字符串、自定义前缀

--- ssTools/ssLogger.h
+++ ssTools/ssLogger.h
@@ -1,4 +1,4 @@
-//@ Project : ssTools
+﻿//@ Project : ssTools
 //@ Copyright : Non-unruly (Git)
 //@ Git: https://github.com/Non-Unruly/ssTools
 //@
@@ -39,6 +39,7 @@
 #include "ssTools.h"
 
 #define ssLoggerOpen(_path, _log_name, _level, _sync) ssLogger::init(_path, _log_name, _level, _sync)
+#define ssLoggerFlags(flags) ssLogger::SetLogPrefix(flags)
 
 #define ssloggerDebug(...) ssLogger::output(true, 1, __FILE__, __FUNCTION__, __LINE__, __VA_ARGS__)
 #define ssloggerInfo(...) ssLogger::output(true, 2, __FILE__, __FUNCTION__, __LINE__, __VA_ARGS__)
@@ -52,12 +53,12 @@
 #define ssloggerError_np(...) ssLogger::output(false, 4, __FILE__, __FUNCTION__, __LINE__, __VA_ARGS__)
 #define ssloggerDisaster_np(...) ssLogger::output(false, 5, __FILE__, __FUNCTION__, __LINE__, __VA_ARGS__)
 
-#define TAG_TIME 0b00001
-#define TAG_LEVEL 0b00010
-#define TAG_FILE 0b00100
-#define TAG_FUNC 0b01000
-#define TAG_LINE 0b10000
-#define TAG_ALL 0b11111
+#define PREFIX_TIME 0b00001	//时间戳
+#define PREFIX_LEVEL 0b00010	//等级字符串
+#define PREFIX_FILE 0b00100	//文件名
+#define PREFIX_FUNC 0b01000	//函数名
+#define PREFIX_LINE 0b10000	//行数
+#define PREFIX_ALL 0b11111		//全部
 
 class ssLogger
 {
@@ -80,40 +81,57 @@ public:
 	// maxLen 单条日志最大长度 one log record string's length
 	// level 日志精准等级[0~5] 等级越高，输出的日志越精确，日志量越少 the level of log , high level and fewer log and more accurate log
 	// tag 日志输出的前缀字段，时间（精确至毫秒）、日志等级、触发文件、触发函数、触发行号 the log prefix about time(ms),level,file,func,line number
-	// sync 同步日志开关 the sync/async mode of log , default is async(false)
+	// sync 同步日志开关 the sync/async mode of log , default is sync(true)
 	static bool init(const char *_logPath, const char *_name, LOG_LEVEL _level = LEVEL_ALL, bool sync = true);
 
-	//添加日志字符串
+	// 添加日志字符串
 	// print 是否输出到屏幕 whether the text is displayed on the screen
 	// type 日志类型映射为等级 log's type/level (enum LOG_LEVEL)
 	// functionName 当前所在函数的函数名 log's function name
 	// line 日志点行数 log's line number
 	// format 日志格式 format , support %d %ld %s %c %f %lf(%.8lf) %x(0x%02x)
-	static void output(bool print, int type, const char *srcName, const char *functionName, int line, const char *format, ...);
+	// return log text without preffix
+	static std::string output(bool print, int type, const char *srcName, const char *functionName, int line, const char *format, ...);
+
+	// 设置日志的前缀
+	static void SetLogPrefix(int flags);
 
 	typedef struct _SSLOG_INFO_T_
 	{
+		std::string prefix;
+		bool isPrefix;//是否显示前缀
+		std::string text;
+		std::string log;
+
 		std::string time;
 		std::string type;
 		std::string file;
 		std::string function;
 		int line;
-		std::string log;
+
 		bool isPrint;
 		_SSLOG_INFO_T_()
 		{
 			isPrint = false;
 		};
-		_SSLOG_INFO_T_(std::string _timetick, std::string _logType, std::string _file, std::string _function, int _line, std::string _logText, bool print = true)
+		_SSLOG_INFO_T_(std::string _prefix, std::string _text, bool _isPrint, bool _isPrefix = true)
 		{
-			time = _timetick;
-			file = _file;
-			function = _function;
-			line = _line;
-			type = _logType;
-			log = _logText;
-			isPrint = print;
+			prefix = _prefix;
+			text = _text;
+			isPrint = _isPrint;
+			isPrefix = _isPrefix;
+			log = prefix + text;
 		};
+		//_SSLOG_INFO_T_(std::string _timetick, std::string _logType, std::string _file, std::string _function, int _line, std::string _logText, bool print = true)
+		//{
+		//	time = _timetick;
+		//	file = _file;
+		//	function = _function;
+		//	line = _line;
+		//	type = _logType;
+		//	log = _logText;
+		//	isPrint = print;
+		//};
 	} ssLog_info_t;
 
 private:
@@ -121,7 +139,8 @@ private:
 	static std::string m_fileName;
 	static bool m_isInit;
 	static bool m_sync;
-	static size_t m_size;
+	static int m_flags;
+	static std::vector<std::string> m_level_map;
 
 #if defined _WIN32
 	static std::mutex m_qMtx;
@@ -132,9 +151,7 @@ private:
 	static std::set<std::string> m_keys;
 
 	static std::string ssFormat(int length, const char *_format, va_list _vlist);
-	static std::string function_line(const char *_src, const char *_function, int _line);
 
-	static std::string show_text(ssLog_info_t info);
 #if defined _WIN32
 	static void m_LogThread();
 #else
