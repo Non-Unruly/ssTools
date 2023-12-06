36b7fd5852f853bb06b29f8648200a84f2f9cd63
Non-Unruly <shihoongbo@163.com>
2023-12-06 10:44:56 +0800

    调整windows获取本地IP v4地址的接口

--- ssTools/ssNetwork.cpp
+++ ssTools/ssNetwork.cpp
@@ -3,43 +3,42 @@
 namespace ssTools
 {
 
-	std::vector<std::string> GetLocalIPv4(std::string &addrs)
+	std::vector<std::string> GetLocalIPv4(std::string &addrs, const char *separator)
 	{
-		std::string ip;
 		std::vector<std::string> ip_lst;
 #ifdef _WIN32
 		// Windows
 		WSADATA wsaData;
-		int iResult = WSAStartup(MAKEWORD(2, 2), &wsaData);
-		if (iResult != 0)
+		int res = WSAStartup(MAKEWORD(2, 2), &wsaData);
+		if (res != 0)
 		{
-			printf("WSAStartup failed with error: %d\n", iResult);
+			std::cout << "winsock initialize failed" << std::endl;
 			return ip_lst;
 		}
 
-		char strName[1024];
-		gethostname(strName, 1024);
+		char hostName[128];
+		int len = gethostname(hostName, 128);
 
-		// ��ȡ���ؼ�������?
-		struct hostent *pHostEnt = gethostbyname(strName);
-		if (pHostEnt == NULL)
+		struct addrinfo nif;
+		memset(&nif, 0, sizeof(nif));
+		nif.ai_family = AF_UNSPEC;
+		nif.ai_socktype = SOCK_STREAM;
+		nif.ai_protocol = IPPROTO_TCP;
+
+		struct addrinfo *addrInfos = nullptr;
+		getaddrinfo(hostName, "", &nif, &addrInfos);
+		while (true)
 		{
-			return ip_lst;
+			if (addrInfos == nullptr)
+				break;
+			struct sockaddr_in *addr = (struct sockaddr_in *)addrInfos->ai_addr;
+			char *ip = inet_ntoa(addr->sin_addr);
+			std::string ipstr = std::string(ip);
+			if (ipstr != "0.0.0.0" && ipstr != "127.0.0.1")
+				ip_lst.push_back(std::string(ip));
+			addrInfos = addrInfos->ai_next;
 		}
-
-		// ��ȡ���ؼ����IP��ַ
-		int n = 0;
-		while (pHostEnt->h_addr_list[n] != NULL)
-		{
-			char tt[1024];
-			sprintf(tt, "%d.%d.%d.%d\0", pHostEnt->h_addr_list[n][0] & 0x00FF, pHostEnt->h_addr_list[n][1] & 0x00FF, pHostEnt->h_addr_list[n][2] & 0x00FF, pHostEnt->h_addr_list[n][3] & 0x00FF);
-			ip_lst.push_back(std::string(tt));
-			n++;
-		}
-
 		WSACleanup();
-
-		addrs = ss_keyJoint(ip_lst, ",");
 #else
 		// Linux
 		struct ifaddrs *ifAddrStruct = NULL;
@@ -71,81 +70,13 @@ namespace ssTools
 			// }
 			ifAddrStruct = ifAddrStruct->ifa_next;
 		}
+#endif
 		for (int i = 0; i < ip_lst.size(); i++)
 		{
 			addrs += ip_lst[i];
 			if (i < ip_lst.size() - 1)
-				addrs += ",";
+				addrs += std::string(separator);
 		}
-
-#endif
-
 		return ip_lst;
 	}
 }
-//
-// #include <iostream>
-// #include <string>
-// #include <Winsock2.h>
-// #include <sstream>
-//
-// using namespace std;
-//
-// #pragma comment(lib, "ws2_32.lib")
-//
-// string	GetLocalIP()
-//{
-//	string strIP("127.0.0.1");
-//
-//	//��ʼ��sock
-//	WSADATA WSAData;
-//	if (WSAStartup(MAKEWORD(2, 0), &WSAData) != 0)
-//	{
-//		return strIP;
-//	}
-//
-//	//��ȡ���ؼ����������?
-//	char	strName[1024];
-//	gethostname(strName, 1024);
-//
-//	//��ȡ���ؼ�������?
-//	struct hostent *pHostEnt = gethostbyname(strName);
-//	if (pHostEnt == NULL)
-//	{
-//		return strIP;
-//	}
-//
-//	//��ȡ���ؼ����IP��ַ
-//	int n = 0;
-//	while (pHostEnt->h_addr_list[n] != NULL)
-//	{
-//		stringstream ss;
-//		ss << (pHostEnt->h_addr_list[n][0] & 0x00FF) << "."
-//			<< (pHostEnt->h_addr_list[n][1] & 0x00FF) << "."
-//			<< (pHostEnt->h_addr_list[n][2] & 0x00FF) << "."
-//			<< (pHostEnt->h_addr_list[n][3] & 0x00FF) << ends;
-//
-//		string strTemp(ss.str());
-//
-//
-//		if (string::npos != strTemp.find("192.168.1."))
-//		{
-//			strIP = strTemp;
-//			break;
-//		}
-//		else
-//		{
-//			n++;
-//		}
-//	}
-//
-//	WSACleanup();
-//	return strIP;
-//}
-//
-// int main()
-//{
-//	cout << GetLocalIP() << endl;
-//
-//	return 0;
-//}
